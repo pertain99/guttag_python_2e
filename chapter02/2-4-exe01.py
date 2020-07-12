26df6c1214db4f20b6aad7ded106962af43993a6
Ning Ma <maning306@hotmail.com>
2020-07-12 16:59:06 -0400

    my solution

--- chapter02/2-4-exe01.py
+++ chapter02/2-4-exe01.py
@@ -10,10 +10,16 @@ y = float(y)
 z = float(z)
 
 # test the numbers against each other and prints the greatest
-if (x >= y and x >= z):
-    print("{} is the greatest number".format(x))
-elif (y >= x and y>=z ):
-    print("{} is the greatest number".format(y))
-else:
-    print("{} is the greatest number".format(z))
-
+# if (x >= y and x >= z):
+#     print("{} is the greatest number".format(x))
+# elif (y >= x and y>=z ):
+#     print("{} is the greatest number".format(y))
+# else:
+#     print("{} is the greatest number".format(z))
+#
+max = x
+if y > max:
+    max = y
+if z > max:
+    max = z
+print("{} is the greatest number".format(max))
