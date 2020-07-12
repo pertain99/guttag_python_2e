7a2ee508906de78e0f24ca61edd5dd42a1bdf529
Ning Ma <maning306@hotmail.com>
2020-07-12 16:41:37 -0400

    my solution

---
+++ chapter02/2-4-exe01.py
@@ -0,0 +1,19 @@
+# takes three numbers form user input then prints out the greatest number
+
+# take input
+x = input("Input the first number please: ")
+y = input("Input the second number please: ")
+z = input("Input the third number please: ")
+
+x = float(x)
+y = float(y)
+z = float(z)
+
+# test the numbers against each other and prints the greatest
+if (x >= y and x >= z):
+    print("{} is the greatest number".format(x))
+elif (y >= x and y>=z ):
+    print("{} is the greatest number".format(y))
+else:
+    print("{} is the greatest number".format(z))
+
