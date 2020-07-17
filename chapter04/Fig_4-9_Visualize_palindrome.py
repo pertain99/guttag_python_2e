20a9c29d6fae3d4596911098688dddae0b6cb8c2
Ning Ma <maning306@hotmail.com>
2020-07-17 11:43:31 -0400

    my solution

---
+++ chapter04/Fig_4-9_Visualize_palindrome.py
@@ -0,0 +1,32 @@
+def isPalindrome(s):
+    """Assume s is a str
+       Returns True if s is a palindrome; False otherwise.
+       Punctuation marks, blanks, and capitalization are ignored."""
+
+    def toChar(s):
+        s = s.lower()
+        letters = ''
+        for c in s:
+            if c in 'abcdefghijklmnopqrstuvwxyz':
+                letters = letters + c
+        return letters
+
+    def isPal(s):
+        print('    isPal called with', s)
+        if len(s) <= 1:
+            print('    About to return True from base case')
+            return True
+        else:
+            answer = s[0] == s[-1] and isPal(s[1:-1])
+            print('    About to return', answer, 'for', s)
+            return answer
+
+    return isPal(toChar(s))
+
+def testIsPalindrome():
+    print('Try dogGod')
+    print(isPalindrome('dogGod'))
+    print('Try doGood')
+    print(isPalindrome('doGood'))
+
+testIsPalindrome()
